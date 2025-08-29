12691762853afd314cc30a89a7a4bf6ca6520f69
25730032.Đinh Văn Huy <25730032@ms.uit.edu.vn>
2025-08-29 15:20:16 +0700

    feat: create piece class

--- src/piece.h
+++ src/piece.h
@@ -5,4 +5,14 @@
 #ifndef TETRIS_GAME_PIECE_H
 #define TETRIS_GAME_PIECE_H
 
-#endif //TETRIS_GAME_PIECE_H
+class Piece {
+public:
+  int row{0};
+  int col{0};
+
+private:
+  int id_{0};
+  int rotation_{0};
+};
+
+#endif // TETRIS_GAME_PIECE_H
