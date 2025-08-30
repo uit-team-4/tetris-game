52f748305997e84ae748e11bf43862c88e95af1d
25730032.Đinh Văn Huy <25730032@ms.uit.edu.vn>
2025-08-30 10:58:30 +0700

    feat: reset grid.cpp grid.h

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,13 +1,12 @@
 #include "grid.h"
 #include "colors.h"
-#include <iostream>
 
 Grid::Grid() {
   numRows = 20;
   numCols = 10;
   cellSize = 30;
   Initialize();
-  // colors = GetCellColors();
+  colors = GetCellColors();
 }
 
 void Grid::Initialize() {
@@ -18,15 +17,6 @@ void Grid::Initialize() {
   }
 }
 
-void Grid::Print() {
-  for (int row = 0; row < numRows; row++) {
-    for (int column = 0; column < numCols; column++) {
-      std::cout << grid[row][column] << " ";
-    }
-    std::cout << std::endl;
-  }
-}
-
 void Grid::Draw() {
   for (int row = 0; row < numRows; row++) {
     for (int column = 0; column < numCols; column++) {
@@ -34,54 +24,5 @@ void Grid::Draw() {
       DrawRectangle(column * cellSize + 11, row * cellSize + 11, cellSize - 1,
                     cellSize - 1, colors[cellValue]);
     }
-  }
-}
-
-bool Grid::IsCellOutside(int row, int column) {
-  if (row >= 0 && row < numRows && column >= 0 && column < numCols) {
-    return false;
-  }
-  return true;
-}
-
-bool Grid::IsCellEmpty(int row, int column) {
-  if (grid[row][column] == 0) {
-    return true;
-  }
-  return false;
-}
-
-int Grid::ClearFullRows() {
-  int completed = 0;
-  for (int row = numRows - 1; row >= 0; row--) {
-    if (IsRowFull(row)) {
-      ClearRow(row);
-      completed++;
-    } else if (completed > 0) {
-      MoveRowDown(row, completed);
-    }
-  }
-  return completed;
-}
-
-bool Grid::IsRowFull(int row) {
-  for (int column = 0; column < numCols; column++) {
-    if (grid[row][column] == 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-void Grid::ClearRow(int row) {
-  for (int column = 0; column < numCols; column++) {
-    grid[row][column] = 0;
-  }
-}
-
-void Grid::MoveRowDown(int row, int numRows) {
-  for (int column = 0; column < numCols; column++) {
-    grid[row + numRows][column] = grid[row][column];
-    grid[row][column] = 0;
   }
 }
